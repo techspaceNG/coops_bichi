dd7d08ed77f95c0ac2b3131db741aed4d7ec7df0
ada12u <adaok12@gmail.com>
2026-01-02 14:09:41 +0100

    Enable SSL for Database connections (Required for Aiven)

--- app/helpers/Database.php
+++ app/helpers/Database.php
@@ -61,6 +61,8 @@ class Database
                 PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
                 PDO::ATTR_EMULATE_PREPARES   => false,
+                PDO::MYSQL_ATTR_SSL_CA       => true,
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
             ],
         ];
     }
